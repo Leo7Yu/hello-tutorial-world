f6f422bc208258463be43301d797a9e7296d855d
yujianfeng <yujianfeng@jf180.cn>
2017-02-16 11:15:37 +0800

    add fold

---
+++ hello-world-app/src/main/java/cn/leo/pattern/Main.java
@@ -0,0 +1,21 @@
+package cn.leo.pattern;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+	private static Pattern cardnoErrorPattern = Pattern.compile("卡\\[([0-9]{19})\\](验证|充值)失败");
+	
+	public static void main(String[] args) {
+		String message="加油卡[1000114400018914163]充值失败!";
+		
+		Matcher matcher =cardnoErrorPattern.matcher(message);
+		if(matcher.find()){
+			System.out.println("find");
+		}else{
+			System.out.println("not find");
+		}
+	}
+
+}
