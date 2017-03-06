067688fff20703ce02f17a441cadf483c953b514
yujianfeng <yujianfeng@jf180.cn>
2017-03-06 10:40:27 +0800

    add template

--- hello-world-app/src/main/java/cn/leo/Main.java
+++ hello-world-app/src/main/java/cn/leo/Main.java
@@ -3,19 +3,7 @@ package cn.leo;
 public class Main {
 
 	public static void main(String[] args) {
-		String remark="槽9第1张|10086,yecx";
-		String[] config=remark.split("\\|");
-		if(config.length<2){
-			System.out.println("splite | error:"+remark);
-		}
-		remark=config[1];
-		config=remark.split(",");
-		if(config.length<2){
-			System.out.println("splite , error:"+remark);
-		}
-		for (String string : config) {
-			System.out.println(string);
-		}
+		System.out.println(String.format("http://sinopec.jf180.cn/sino/charge/sms/send/api?cardNo=%s", 111));
 	}
 
 }
