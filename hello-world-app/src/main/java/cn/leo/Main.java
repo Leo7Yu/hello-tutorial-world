d357642beea46adce736b9b7a42f25d4ac09f194
于建峰 <yujianfeng@jf180.cn>
2017-03-10 11:40:03 +0800

    创建签名

--- hello-world-app/src/main/java/cn/leo/Main.java
+++ hello-world-app/src/main/java/cn/leo/Main.java
@@ -3,7 +3,25 @@ package cn.leo;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(String.format("http://sinopec.jf180.cn/sino/charge/sms/send/api?cardNo=%s", 111));
+		String s="123";
+		int si=Integer.parseInt(s);
+		int i=123;
+		Integer I=123;
+		if(s.equals(i)){
+			System.out.println("s=i");
+		}else{
+			System.out.println("s!=i");
+		}
+		
+		if(s.equals(I)){
+			System.out.println("s=I");
+		}else{
+			System.out.println("s!=I");
+		}
+		
+		if(si==i){
+			System.out.println("si=i");
+		}
 	}
 
 }
---
+++ hello-world-app/src/main/java/cn/leo/sign/CheckSignMain.java
@@ -0,0 +1,53 @@
+package cn.leo.sign;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CheckSignMain {
+	private static final List<String> EXCLUDE_SIGN_PARAMETER_NAMES = Arrays.asList(new String[] { "sig" });
+	private static final String PRIVATE_KEY = "cWvJj78B6E4QhcumOfaU0dpB!GXu$Rfe";// 32位密钥
+
+	public static void main(String[] args) {
+
+		Map<String, Object> params = new HashMap<>();
+		params.put("eid", "10000"); // 企业标识
+		params.put("orderno", "201703100000001");// 企业订单号
+
+		String sig = getGig(params);
+		System.out.println(sig);
+	}
+
+	private static String getGig(Map<String, Object> params) {
+		StringBuilder sBuilder = new StringBuilder("eorder");
+
+		List<String> paramNames = getSortedParameterNames(params);
+		for (String name : paramNames) {
+			sBuilder.append(name);
+			sBuilder.append(params.get(name));
+		}
+		
+		sBuilder.append(PRIVATE_KEY);
+		
+		String plainSign=sBuilder.toString();
+		System.out.println("plain sig:"+plainSign);
+		String sig=MD5Utils.md5(plainSign);
+
+		return sig;
+	}
+
+	private static List<String> getSortedParameterNames(Map<String, Object> params) {
+		List<String> keys = new ArrayList<String>();
+		for (String key : params.keySet()) {
+			if (EXCLUDE_SIGN_PARAMETER_NAMES.contains(key)) {
+				continue;
+			}
+			keys.add(key);
+		}
+		Collections.sort(keys);
+		return keys;
+	}
+}
